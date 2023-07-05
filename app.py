d1d6212eefa41cfd6c00d6f64bf439820fd00988
ryouya <r.hatafuku.sys22@morijiyobi.ac.jp>
2023-07-05 10:44:37 +0900

    aaa

--- app.py
+++ app.py
@@ -1,75 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import db, string, random
-from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 
-@app.route('/', methods=['GET'])
-def index():
-    msg = request.args.get('msg')
-    
-    if msg == None:
-        return render_template('index.html')
+@app.route('/')
+def top():
+    tweets = db.get_tweets()
+    return render_template('top.html', tweets=tweets)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        db.create_user(username, email, password)
+        return redirect(url_for('top'))
     else:
-        return render_template('index.html', msg=msg)
-    
-    return render_template('index.html')
+        return render_template('signup.html')
 
-@app.route('/', methods=['POST'])
-def login():
-    user_name = request.form.get('username')
-    password = request.form.get('password')
-
-    if db.login(user_name, password):
-        session['user'] = True
-        session.permanent = True
-        app.permanent_session_lifetime = timedelta(minutes=30)
-        return redirect(url_for('mypage'))
-    else :
-        error = 'ログインに失敗しました。'
-        
-        input_data = {'user_name':user_name, 'password':password}
-        return render_template('index.html', error=error, data=input_data)
-
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
-
-@app.route('/mypage', methods=['GET'])
-def mypage():
-    if 'user' in session:
-        return render_template('mypage.html')
-    else:
-        return redirect(url_for('index'))
-
-@app.route('/register')
-def register_form():
-    return render_template('register.html')
-
-@app.route('/register_exe', methods=['POST'])
-def register_exe():
-    user_name = request.form.get('user_name')
-    password = request.form.get('password')
-    
-    
-    if user_name == '':
-        error = 'ユーザー名が入力されていません'
-        return render_template('register.html', error=error)
-    if password == '':
-        error = 'パスワードが入力されていません'
-        return render_template('register.html', error=error)
-    
-    count = db.insert_user(user_name, password)
-    
-    if count == 1:
-        msg = '登録が完了しました'
-        return redirect(url_for('index', msg=msg))
-    else:
-        error = '登録に失敗しました'
-        return render_template('register.html', error=error)
-
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    tweet_text = request.form.get('tweet-text')
+    db.post_tweet(session['user_id'], tweet_text)
+    return redirect(url_for('top'))
 
 if __name__ == '__main__':
     app.run(debug=True)
